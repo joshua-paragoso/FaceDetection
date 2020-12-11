92842757ddce3bd35d53782c00547f6ea003a9b7
joshua-paragoso <paragoso90@gmail.com>
2020-12-11 09:05:25 -1000

    program now updates frames to sjhow real time video capturing

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -1,3 +1,33 @@
 import cv2
 
-print("Whats up")
+# face classifier
+# some kind input in classifier, output would give a 0 (no) or 1 (yes)
+# smile or not smile
+# finds faces to find smile
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#grab webcam feed
+webcam = cv2.VideoCapture(0)
+#shows the current frame
+#after getting webcam variable
+while True:
+
+    succesful_frame_read, frame = webcam.read()
+
+# #name of window
+# # show image to screen
+    cv2.imshow('Smile detector', frame)
+
+# # keeps webcam open till key is pressed
+# # stays on with real time
+# # display
+    cv2.waitKey()
+
+# # release webcam
+# # let OS know the app is done with webcam
+webcam.release()
+
+# # closes all windows
+cv2.destroyAllWindows()
+
+print("code complete")
